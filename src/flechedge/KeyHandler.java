272a2db29f4d628ce6afcedb86d1f37c9828791b
Maxwell Giancaterino <mgianc@seas.upenn.edu>
2017-06-20 17:58:00 -0400

    Refined KeyHandler

--- src/flechedge/KeyHandler.java
+++ src/flechedge/KeyHandler.java
@@ -1,12 +1,25 @@
 package flechedge;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
 
 public class KeyHandler implements EventHandler<KeyEvent>{
-
-	public void handle(KeyEvent event) {
-		
+	
+	private Set<String> keyInputs = new HashSet<String>();  
+	
+	public void handle(KeyEvent e) {
+		if (e.getEventType() == KeyEvent.KEY_PRESSED) {
+			keyInputs.add(e.getText());
+		}
+		if (e.getEventType() == KeyEvent.KEY_RELEASED) {
+			keyInputs.remove(e.getText());
+		}
+		for (String s : keyInputs) {
+			System.out.println(s + "\n");
+		}
 	}
 
 }
