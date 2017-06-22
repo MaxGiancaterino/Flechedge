17f2b3aaa26bcd8478e98f4cbd63773bd5a96bf7
Maxwell Giancaterino <mgianc@seas.upenn.edu>
2017-06-22 12:27:05 -0400

    Convert HashMap to EnumMap

--- src/flechedge/CommandParser.java
+++ src/flechedge/CommandParser.java
@@ -1,18 +1,22 @@
 package flechedge;
 
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class CommandParser {
 	
+	public enum BaseCommands {
+		P1_MOD, P1_RET, P1_PAR, P1_ADV, P1_HIGH, P1_MID, P1_LOW, P1_EXT, P1_THROW
+	}
+	
 	private Set<String> keyInputs = new HashSet<String>();
-	private Map<String, String> keyMap = new HashMap<String, String>();
+	private Map<BaseCommands, String> keyMap = new EnumMap<BaseCommands, String>(BaseCommands.class);
 	private Sprite p1 = new Sprite();
 	private double speed;
 	
-	public CommandParser(Sprite p1, double speed, Set<String> keyInputs, Map<String, String> keyMap) {
+	public CommandParser(Sprite p1, double speed, Set<String> keyInputs, Map<BaseCommands, String> keyMap) {
 		this.keyInputs = keyInputs;
 		this.keyMap = keyMap;
 		this.p1 = p1;
@@ -20,35 +24,31 @@ public class CommandParser {
 	}
 	
 	public void parse() {
-<<<<<<< HEAD
-		if(keyInputs.contains("UP")) {
-=======
-		if(keyInputs.contains(keyMap.get("p1Mod"))) {
+		if(keyInputs.contains(keyMap.get(BaseCommands.P1_MOD))) {
 			p1.addVelocity(0, -speed);
->>>>>>> a832af573765bd5832b929802b2ba4ed2d147eae
 		}
-		if(keyInputs.contains(keyMap.get("p1Ret"))) {
+		if(keyInputs.contains(keyMap.get(BaseCommands.P1_RET))) {
 			p1.addVelocity(-speed, 0);
 		}
-		if(keyInputs.contains(keyMap.get("p1Par"))) {
+		if(keyInputs.contains(keyMap.get(BaseCommands.P1_PAR))) {
 			p1.addVelocity(0, speed);
 		}
-		if(keyInputs.contains(keyMap.get("p1Adv"))) {
+		if(keyInputs.contains(keyMap.get(BaseCommands.P1_ADV))) {
 			p1.addVelocity(speed, 0);
 		}
-		if(keyInputs.contains(keyMap.get("p1High"))) {
+		if(keyInputs.contains(keyMap.get(BaseCommands.P1_HIGH))) {
 			
 		}
-		if(keyInputs.contains(keyMap.get("p1Mid"))) {
+		if(keyInputs.contains(keyMap.get(BaseCommands.P1_MID))) {
 			
 		}
-		if(keyInputs.contains(keyMap.get("p1Low"))) {
+		if(keyInputs.contains(keyMap.get(BaseCommands.P1_LOW))) {
 			
 		}
-		if(keyInputs.contains(keyMap.get("p1Ext"))) {
+		if(keyInputs.contains(keyMap.get(BaseCommands.P1_EXT))) {
 			
 		}
-		if(keyInputs.contains(keyMap.get("p1Throw"))) {
+		if(keyInputs.contains(keyMap.get(BaseCommands.P1_THROW))) {
 			
 		}
 	}
