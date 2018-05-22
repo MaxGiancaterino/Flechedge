2726a143551256a99d9b4e09622511aee4d7d5fa
Crytera <nanderson321@gmail.com>
2018-05-21 22:17:18 -0400

    woops

--- src/flechedge/BetterDuelist.java
+++ src/flechedge/BetterDuelist.java
@@ -1,5 +1,35 @@
 package flechedge;
 
-public class BetterDuelist {
+import javafx.scene.image.ImageView;
 
+public class BetterDuelist{
+	private int direction;
+	private ImageView imageView;
+
+	public BetterDuelist(int direction){
+		this.direction = direction;
+	}
+	
+	public void advance() {
+	}
+	
+	public void retreat() {
+	}
+	
+	public void extend() {
+	}
+	
+	public void high() {
+	}
+	
+	public void mid() {
+	}
+	
+	public void low() {
+	}
+	
+	public void lunge() {
+	}
+	
+	//etc.
 }
